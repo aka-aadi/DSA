f1f80356719490de5ce171aa8857299778612bc4
Aaditya R Narayan <59530597+aka-aadi@users.noreply.github.com>
2024-07-24 21:15:57 +0530

    Update two_stack_using array.py

--- two_stack_using array.py
+++ two_stack_using array.py
@@ -1,12 +1,11 @@
 arr1=[]
-capt=10
+capt=1000
 top1=0
 top2=-1
-cap1=10//2
-cap2=-(10//2)
+cap1=1000//2
+cap2=-(1000//2)
 for i in range(capt):
     arr1.append(None)
-print(arr1)
 
 l1=int(input())
 n=list(map(int,input().split()))
@@ -41,33 +40,37 @@ for m in range(0,top1+1):
 
 for n in range(-1,top2-1,-1):
     ctr2+=1
-    
-print("Stack 1 Element: ")
+
+print("Stack 1 Elements: ")
 for x in range(top1,-1,-1):
     print(arr1[x],end=" ")
 
-print("\nStack 2 Element: ")
+print("\nStack 2 Elements: ")
 for y in range(top2,-1+1):
     print(arr1[y],end=" ")
 
 if pop1>ctr1:
     print("\nStack underflow. pop from stack 1 failed")
-    for a in range(0,top1+1):
-        arr1.remove(arr1[top1])
-        top1-=1
-        ctr1-=1
-        
-print("Stack 1 Element: ")
+    
+    for a in range(pop1):
+        if top1>=0:
+            arr1.remove(arr1[top1])
+            top1-=1
+            ctr1-=1
+    #top1=0
+print("Stack 1 Elements: ")
 for x in range(top1,-1,-1):
     print(arr1[x],end=" ")
 
 if pop2>ctr2:
-    print("Stack underflow. pop from stack 1 failed") 
-    for b in range(-1,top2-1,-1):
+    print("Stack underflow. pop from stack 1 failed")
+else:
+    for b in range(pop2):
         arr1.remove(arr1[top2])
         top2+=1
         ctr2-=1
-        
-print("\nStack 2 Element: ")
-for y in range(top2,-1):
+
+print("\nStack 2 Elements: ")
+for y in range(top2,-1+1):
     print(arr1[y],end=" ")
+
